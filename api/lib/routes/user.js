13be7d12a893ef665bed68a813a65e1a9496e37e
Javier <javierbelbruno@gmail.com>
2022-03-05 12:27:05 -0300

    Stopped tracking build File

--- api/lib/routes/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_1 = require("../controllers/user");
-function userRouter(app) {
-    app.post('/api/users/:id/name/:name', user_1.createUser);
-    app.get('/api/users/:id', user_1.searchUser);
-}
-exports.default = userRouter;
-//# sourceMappingURL=user.js.map
